b709b6605fd950a7ce243e4c426199352ebe33dd
mitani1993 <fytim55@gmail.com>
2021-05-16 16:00:55 +0900

    ログイン

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,7 @@ Rails.application.routes.draw do
     root "top#index"
     get "login", to: "sessions#new", as: :login
     post "session", to: "sessions#create", as: :session
-    delete "session", to: "session#destroy"
+    delete "session", to: "sessions#destroy"
   end
 
   namespace :admin do
